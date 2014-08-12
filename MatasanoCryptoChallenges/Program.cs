ac65f4bbee7e4fc8d1f1f2c090844f36c5c8d569
Panayotis Trapatsas <trapatsas@gmail.com>
2014-08-12 16:49:01 +0300

    Ready for challenge #5

--- MatasanoCryptoChallenges/Program.cs
+++ MatasanoCryptoChallenges/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,26 +12,87 @@ namespace MatasanoCryptoChallenges
     {
         static void Main(string[] args)
         {
+            // Set 1 - Challenge 1
+            Console.WriteLine("");
+            Console.WriteLine("Set 1 - Challenge 1");
+            Console.WriteLine("===================");
+            // Solution
+            var s01c01 = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+            Console.WriteLine(string.Format("{0}", ConvertString2Base64(ConvertHex2String(s01c01))));
+
+            // Set 1 - Challenge 2
+            Console.WriteLine("");
+            Console.WriteLine("Set 1 - Challenge 2");
+            Console.WriteLine("===================");
+            // Solution
+            var s01c02 = "1c0111001f010100061a024b53535009181c";
+            var s01c02_b = "686974207468652062756c6c277320657965";
+            Console.WriteLine(string.Format("{0}", GetXOR(ConvertHex2String(s01c02), ConvertHex2String(s01c02_b))));
+
+            // Set 1 - Challenge 3
+            Console.WriteLine("");
+            Console.WriteLine("Set 1 - Challenge 3");
+            Console.WriteLine("===================");
+            // Solution
+            var s01c03 = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+            foreach (var item in SingleByteXORDeCipher(ConvertHex2String(s01c03)))
+            {
+                Console.WriteLine(item);
+            }
+
+            // Set 1 - Challenge 4
+            Console.WriteLine("");
+            Console.WriteLine("Set 1 - Challenge 4");
+            Console.WriteLine("===================");
+            // Solution
+            string fileLocation = "http://cryptopals.com/static/challenge-data/4.txt";
+            WebClient client = new WebClient();
+            using (var stream = client.OpenRead(fileLocation))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        foreach (var item in SingleByteXORDeCipher(ConvertHex2String(line)))
+                        {
+                                Console.WriteLine(item);
+                        }
+                    }
+                }
+            }
+
+            // Set 1 - Challenge 5
+            Console.WriteLine("");
+            Console.WriteLine("Set 1 - Challenge 5");
+            Console.WriteLine("===================");
+            // Solution
+
+
+            // END
             Console.ReadKey();
         }
 
-        private static void SingleByteXORDeCipher(string ResultText)
+        private static IEnumerable<string> SingleByteXORDeCipher(string ResultText)
         {
             for (char SingleChar = '0'; SingleChar <= 'z'; SingleChar++)
             {
                 string phrase = SingleCharXOR(ResultText, SingleChar);
-                Console.WriteLine(SingleChar + ". : " + phrase + " | " + CalcLetterFreqs(phrase));
+                if (!NonsenseFilter(phrase))
+                {
+                    yield return string.Format("Char: \"{0}\", Phrase: \"{1}\"", SingleChar, phrase); //SingleChar + ". : " + phrase + " | " + CalcLetterFreqs(phrase);                
+                }
             }
         }
 
         private static string SingleCharXOR(string ResultText, char SingleChar)
         {
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < ResultText.Length; i++)
-                {
-                    sb.Append((char)(ResultText[i] ^ SingleChar));
-                }
-                return string.Format("{0}", sb);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < ResultText.Length; i++)
+            {
+                sb.Append((char)(ResultText[i] ^ SingleChar));
+            }
+            return string.Format("{0}", sb);
         }
 
         private static string GetXOR(string str01, string str02)
@@ -52,7 +115,7 @@ namespace MatasanoCryptoChallenges
                 string hexOutput = String.Format("{0:X}", value);
                 sb.Append(hexOutput);
             }
-            return string.Format("{0}",sb);
+            return string.Format("{0}", sb);
         }
 
         private static string ConvertHex2String(string HexValues)
@@ -83,17 +146,25 @@ namespace MatasanoCryptoChallenges
         {
             var freqs = input
                 .Where(c => Char.IsLetter(c))
-                .GroupBy(c => Char.ToUpper(c))                    
+                .GroupBy(c => Char.ToUpper(c))
                 .ToDictionary(g => g.Key, g => g.Count());
 
             System.Text.StringBuilder sb = new System.Text.StringBuilder();
 
             foreach (var item in freqs.OrderByDescending(x => x.Value))
             {
-                sb.Append(string.Format("{0}",item.Key,item.Value));
+                sb.Append(string.Format("{0}", item.Key, item.Value));
             }
             var result = string.Format("{0}", sb);
             return result.Substring(0, Math.Min(result.Length, 7));
+        }
+
+        private static bool NonsenseFilter(string text)
+        {
+            const int MaxAnsiCode = 122;
+            const int MinAnsiCode = 65;
+            string s = text.Replace(" ", string.Empty).Replace("\r", string.Empty).Replace("\n", string.Empty);
+            return s.Any(c => c > MaxAnsiCode || c < MinAnsiCode);
         }
     }
 }
